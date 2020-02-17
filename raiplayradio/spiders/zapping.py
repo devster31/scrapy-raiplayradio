f6a07144a5e99f5ea7e7b8c27acb11f55ab9d74c
Daniele Riccucci <daniele.riccucci@gmail.com>
2020-02-17 23:57:45 +0100

    clean comments from spider

--- raiplayradio/spiders/zapping.py
+++ raiplayradio/spiders/zapping.py
@@ -12,38 +12,6 @@ class Zapping(scrapy.Spider):
     seasons = []
 
     def parse(self, response):
-        # self.cur_season = int(
-        #     re.findall(
-        #         r"(\d{4})",
-        #         response.css("ul.listaStagioniPuntate>li.active>a::text").get()
-        #     )[0]
-        # )
-        # self.seasons.append(self.cur_season)
-
-        # self.cur_page = int(
-        #     re.findall(
-        #         r"(\d{1,})",
-        #         response.css("ul.pagination>li.active>a::text").get()
-        #     )
-        # )
-        # self.pages.append(self.cur_page)
-
-        # for episode in response.css(".archivioPuntate").css("div.row.listaAudio"):
-        #     yield {
-        #         "title": episode.css("h3>a::text").get(),
-        #         "url": episode.xpath("@data-mediapolis").get(),
-        #         "date": episode.css("span.canale::text").get(),
-        #         "desc": episode.css("p::text").get().strip(),
-        #         "image": response.urljoin(episode.xpath("@data-image").get()),
-        #     }
-
-        # seasons_not_active = list(
-        #     map(
-        #         lambda x: int(re.findall(r"\d{4}", x)[0]),
-        #         response.css("ul.listaStagioniPuntate>li:not(.active)>a::text").getall()
-        #     )
-        # )
-
         seasons = [
             dict(
                 {
@@ -53,13 +21,6 @@ class Zapping(scrapy.Spider):
             )
             for season in response.css("ul.listaStagioniPuntate>li>a")
         ]
-
-        # for next_season in response.css("ul.listaStagioniPuntate>li:not(.active)>a"):
-        #     if next_season not in self.seasons:
-        #         yield response.follow(next_season, self.parse)
-
-        # for next_page in response.css("ul.pagination>li:not(.active)>a"):
-        #     yield response.follow(next_page, self.parse)
 
         for season in seasons:
             yield response.follow(season["url"], callback=self.parse_pages)
