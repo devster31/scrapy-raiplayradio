5b96b0f8c9a5c49bbed9de98a35309577c378ce0
Daniele Riccucci <daniele.riccucci@gmail.com>
2020-02-20 02:13:33 +0100

    lower log level to INFO

--- raiplayradio/settings.py
+++ raiplayradio/settings.py
@@ -14,6 +14,7 @@ BOT_NAME = "raiplayradio"
 SPIDER_MODULES = ["raiplayradio.spiders"]
 NEWSPIDER_MODULE = "raiplayradio.spiders"
 
+LOG_LEVEL = "INFO"
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 USER_AGENT = "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.87 Safari/537.36"
