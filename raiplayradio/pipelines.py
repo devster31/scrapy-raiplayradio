65867a53510cfe24e2e2b31fc371c0683be07fd2
Daniele Riccucci <daniele.riccucci@gmail.com>
2020-02-23 17:58:01 +0100

    fix dates for older episode while retaining warning

--- raiplayradio/pipelines.py
+++ raiplayradio/pipelines.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+import logging
 import re
 from datetime import datetime
 from queue import PriorityQueue
@@ -17,13 +18,25 @@ from raiplayradio.exporters import ZappingExporter
 class DateFixPipeline(object):
     """
     Episodes older than 2017 don't seem to have the right selector for date.
-    The solution is to try to parse it from the name, which usually contains it.
+    This pipeline attempts to parse it from the title, which usually contains it.
     """
 
     def process_item(self, item, spider):
         if not "updated" in item:
-            date = re.match(
-                r".*(?P<date>\d{1,2}\/\d{2}\/\d{4}).*", item["title"]
-            ).group("date")
-            item["updated"] = datetime.strptime(date, "%d/%m/%Y")
+            try:
+                date = re.match(
+                    r".*?(?P<date>\d{1,2}\/\d{2}\/\d{4}).*", item["title"]
+                ).group("date")
+                item["updated"] = datetime.strptime(date, "%d/%m/%Y")
+            except AttributeError as err:
+                spider.log(
+                    "Date malformed while processing\n{}\n...retrying".format(item),
+                    logging.WARNING,
+                )
+                pdate = re.match(
+                    r".*?(?P<date>(?P<day>\d{1,2}) ?\/(?P<month>\d{2})\/(?P<year>\d{4})).*",
+                    item["title"],
+                ).groups()[1:]
+                date = "/".join(list(pdate))
+                item["updated"] = datetime.strptime(date, "%d/%m/%Y")
         return item
